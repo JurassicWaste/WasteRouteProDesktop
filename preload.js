d339535ffbc84dd35883241bb6c2d1b93c5ba1f0
JurassicWaste <info@jurassic-waste.co.uk>
2025-09-29 01:29:21 +0100

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,1 +1,6 @@
-/* reserved for future desktop-API exposure */
+// Minimal preload script
+const { contextBridge } = require("electron");
+
+contextBridge.exposeInMainWorld("wasteApp", {
+  version: "1.0.0"
+});
